0a9afea45983c0475e992e05adbe182383e67016
Витя <>
2016-08-17 17:57:14 +0400

    Initial commit

---
+++ app/src/main/java/ru/rubicon/firebasetest/utils/Config.java
@@ -0,0 +1,8 @@
+package ru.rubicon.firebasetest.utils;
+
+/**
+ * Created by Витя on 17.08.2016.
+ */
+public class Config {
+    public static final String FIREBASE_URL = "https://fir-test-d9c23.firebaseio.com/ ";
+}
